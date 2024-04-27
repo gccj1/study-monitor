11cf82544f6d7ba79bcba472d6d4138f2bfac022
gccj <2191618127@qq.com>
2024-04-27 20:15:23 +0800

    add client and server

---
+++ study_monitor/monitor_clinet/src/main/java/com/example/monitor_clinet/MonitorClinetApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitor_clinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorClinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorClinetApplication.class, args);
+    }
+
+}
---
+++ study_monitor/monitor_clinet/src/test/java/com/example/monitor_clinet/MonitorClinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitor_clinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorClinetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
